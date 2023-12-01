4111fd144e27edeb91d9fe982ae37d82b2654627
Alesandro Dragnev <92319051+alesandro2251@users.noreply.github.com>
2023-12-01 14:43:32 +0200

    Update urls.py

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('foods-add/', views.addFood, name='addFoods'),
     path('foods/', views.get_foods, name='show-foods'),
+    path('',views.index, name='index')
 ]
