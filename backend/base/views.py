8664e2e1b149f7527efa2f060385c3750c90b6f4
Alesandro Dragnev <92319051+alesandro2251@users.noreply.github.com>
2023-12-01 14:43:02 +0200

    Update views.py

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,6 +2,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ListOfFoodSerializer
 from .models import ListOfFood
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
 
 @api_view(["POST"]) 
 def addFood(request):
